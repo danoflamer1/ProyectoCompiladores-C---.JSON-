91692bc25b7b33affec36f9497781d0e24182cec
Daniel <danisanjose60@gmail.com>
2025-07-07 15:44:14 -0400

    Compilador con la parte del analizador lexico completada(O eso espero)

---
+++ ProyectoCompilador/Program.cs
@@ -0,0 +1,36 @@
+﻿using MyCompiler;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string codigo = @"
+class Persona
+{
+    int edad;
+    float altura;
+};
+
+int main()
+{
+    Persona p1(""Daniel"", 25);
+    return 0;
+}
+";
+
+        Lexico lexer = new Lexico();
+        var simbolos = lexer.Analizar(codigo);
+
+        Console.WriteLine("Tabla de símbolos:");
+        foreach (var s in simbolos)
+        {
+            Console.WriteLine($"{s.Token} = '{s.Lexema}'");
+        }
+
+        Console.WriteLine("\nErrores:");
+        foreach (var e in lexer.Errors)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
