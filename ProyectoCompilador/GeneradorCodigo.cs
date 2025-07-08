87bd190b4b13b3aa0b0756615c465a79f3ed1519
Orlando Miguel Llave Ari <stonkill2012@gmail.com>
2025-07-08 00:26:41 -0400

    Sintactico y Generador de codigo

---
+++ ProyectoCompilador/GeneradorCodigo.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MyCompiler
+{
+    internal class GeneradorCodigo
+    {
+        public static string GenerarJSON(Dictionary<string, List<Dictionary<string, object>>> instancias)
+        {
+            var resultado = new
+            {
+                clases = instancias
+            };
+
+            var opciones = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                NumberHandling = JsonNumberHandling.AllowReadingFromString
+            };
+
+            return JsonSerializer.Serialize(resultado, opciones);
+        }
+    }
+}
